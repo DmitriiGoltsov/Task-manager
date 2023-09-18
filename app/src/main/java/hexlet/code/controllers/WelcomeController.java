86e8e69677893c0f904f164f7901944c6482ff38
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-18 15:42:59 +0200

    feat: add first test controller

--- app/src/main/java/hexlet/code/controllers/WelcomeController.java
+++ app/src/main/java/hexlet/code/controllers/WelcomeController.java
@@ -1,16 +1,13 @@
-package hexlet.code.app.controllers;
+package hexlet.code.controllers;
 
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Welcome controller")
+@RestController
 @RequestMapping(path = "/welcome")
 public class WelcomeController {
 
-    @ApiResponse(responseCode = "200", description = "Welcome page has been rendered")
     @GetMapping(path = "")
     public String getGreeting() {
         return "Welcome to Spring";
