5e392dc213ed79a3c6a15fbbc9c41e3826a4edb1
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-18 15:45:39 +0200

    feat: add initial Spring class

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -1,11 +1,15 @@
-package hexlet.code.app;
+package hexlet.code;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;*/
 
+@Slf4j
 @SpringBootApplication
 public class AppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
