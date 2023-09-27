a66afc40bd257d0870553099bf20fe3bd8a67e3e
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-27 17:18:29 +0200

    fix: simplify the mock emails

--- app/src/test/resources/data.sql
+++ app/src/test/resources/data.sql
@@ -4,4 +4,4 @@ VALUES
     ('John', 'Smith', 'john@gmail.com', '12345'),
     ('Jack', 'Doe', 'jack@mail.com', 'qwerty'),
     ('Jassica', 'Simpson', 'jassika@yahoo.com', '0000'),
-    ('Richard', 'Lionheart', 'saladinisafriend@templemail.uk', 'Acra');
+    ('Richard', 'Lionheart', 'Saladdin@gmail.com', 'Acra');
