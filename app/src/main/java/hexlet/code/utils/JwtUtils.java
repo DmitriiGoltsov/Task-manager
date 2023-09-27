d52ecd5f46dc1d4c24c4fd599c519308be249ed5
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-27 17:39:36 +0200

    feat: make the class to work with secret key for jwt in a proper way

--- app/src/main/java/hexlet/code/utils/JwtUtils.java
+++ app/src/main/java/hexlet/code/utils/JwtUtils.java
@@ -1,11 +1,9 @@
-package hexlet.code.config;
+package hexlet.code.utils;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.slf4j.helpers.Util;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +16,8 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
 
-    private String jwtSigningKey = "secret";
+    @Value("${jwt-secret-key}")
+    private String jwtSigningKey;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -39,6 +38,7 @@ public class JwtUtils {
     }
 
     public Claims extractAllClaims(String token) {
+
         return Jwts.parser()
                 .setSigningKey(jwtSigningKey)
                 .parseClaimsJws(token)
