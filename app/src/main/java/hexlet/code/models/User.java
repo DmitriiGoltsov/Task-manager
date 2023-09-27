3ac7383ac48722e5b5eed302a969abbad22d219a
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-27 17:37:45 +0200

    feat: add validation annotations to the class's fields and make imports precise

--- app/src/main/java/hexlet/code/models/User.java
+++ app/src/main/java/hexlet/code/models/User.java
@@ -2,8 +2,17 @@ package hexlet.code.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import hexlet.code.enums.UserRole;
-import lombok.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Id;
@@ -14,10 +23,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import org.springframework.security.core.GrantedAuthority;
 
-import java.sql.Timestamp;
-import java.util.Collection;
+import java.util.Date;
 
 @Entity
 @Table(name = "users")
@@ -34,19 +41,28 @@ public class User {
     @Setter(AccessLevel.NONE)
     private long id;
 
+    @NotBlank(message = "First name cannot be empty")
+    @Column(name = "first_name")
     private String firstName;
 
+    @NotBlank(message = "Last name cannot be empty")
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(unique = true, length = 40, nullable = false)
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email")
     private String email;
 
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 3, max = 255, message = "The password must be 3 to 255 characters long")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password")
     private String password;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    private Timestamp createdAt;
+    @Column(name = "created_at")
+    private Date createdAt;
 
-    private UserRole userRole;
 }
