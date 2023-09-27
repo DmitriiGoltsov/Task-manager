cd5b04cddfca78c7a9bc6187ecb036577a519021
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-27 17:40:18 +0200

    fix: change package of the class

--- app/src/main/java/hexlet/code/utils/Encoders.java
+++ app/src/main/java/hexlet/code/utils/Encoders.java
@@ -1,4 +1,4 @@
-package hexlet.code.config;
+package hexlet.code.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
