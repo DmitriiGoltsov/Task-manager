db35f053b57f5d18e2b6d355ef40b9542ced0836
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-27 17:22:04 +0200

    fix: update WebSecurity class
    1) The name of the class is now better reflects its purpose;
    2) Added String constants that are used in the code, making it more readable;
    3) Added request matchers for urls authentication for whom is not needed

--- app/src/main/java/hexlet/code/config/WebSecurityConfig.java
+++ app/src/main/java/hexlet/code/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package hexlet.code.config;
 
+import hexlet.code.filters.JwtAuthFilter;
 import hexlet.code.services.CustomUserDetailsService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,17 +19,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
+import static hexlet.code.controllers.UsersController.USER_CONTROLLER_PATH;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class WebSecurityConfig {
+
+    public static final List<GrantedAuthority> DEFAULT_AUTHORITIES = List.of(new SimpleGrantedAuthority("USER"));
+    public static final String LOGIN = "/login";
+
+    @Value("${base-url}")
+    private String baseUrl;
 
     private final JwtAuthFilter jwtAuthFilter;
     private final CustomUserDetailsService userDetailsService;
@@ -35,23 +48,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http.csrf(AbstractHttpConfigurer::disable);
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(antMatcher( "/api/login")).permitAll()
+                        .requestMatchers(antMatcher(baseUrl + LOGIN)).permitAll()
+                        .requestMatchers(antMatcher(baseUrl + USER_CONTROLLER_PATH)).permitAll()
                         .anyRequest()
                         .authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .authenticationProvider(authenticationProvider());
 
         return http.build();
     }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return authenticationProvider;
