f53d694d7b1104853b6e3465e15b345ea319a94b
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-27 17:29:02 +0200

    fix: remove unused imports and add String consts.
    1) Remove unused imports;
    2) Add several String constants to class. They are used later in the code, making it more readable

--- app/src/main/java/hexlet/code/controllers/UsersController.java
+++ app/src/main/java/hexlet/code/controllers/UsersController.java
@@ -13,8 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.xml.bind.ValidationException;
 import lombok.AllArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,32 +24,36 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
+import static hexlet.code.controllers.UsersController.USER_CONTROLLER_PATH;
+
 @Tag(name = "User controller")
 @RestController
 @AllArgsConstructor
-@RequestMapping(path = "${base-url}")
+@RequestMapping(path = "${base-url}" + USER_CONTROLLER_PATH)
 public class UsersController {
 
-    @Autowired
+    public static final String USER_CONTROLLER_PATH = "/users";
+    public static final String ID = "/{id}";
+
     private final UserService userService;
 
-    @Operation(summary = "Operation allows to get all users stored in the DB")
+    @Operation(summary = "Operation to get all users stored in the DB")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Users were successfully found and returned"),
+            @ApiResponse(responseCode = "200", description = "Users were successfully found and loaded"),
             @ApiResponse(responseCode = "404", description = "There are not stored users in the DB")
     })
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAllUsers() {
         List<User> result = userService.getAllUsers();
         if (result.isEmpty()) {
             throw new RuntimeException("There are not stored users in the DB");
         }
-        return userService.getAllUsers();
+        return result;
     }
 
     @Operation(summary = "Operation adds a new user to the DB")
     @ApiResponse(responseCode = "201", description = "User was successfully created")
-    @PostMapping("/users")
+    @PostMapping
     public User createUser(@RequestBody UserDTO userDTO) throws ValidationException {
         return userService.createUser(userDTO);
     }
@@ -61,7 +63,7 @@ public class UsersController {
             @ApiResponse(responseCode = "200", description = "User was found"),
             @ApiResponse(responseCode = "404", description = "User with such id has not been found")
     })
-    @GetMapping("/users/{id}")
+    @GetMapping(ID)
     public User getUser(@PathVariable("id") Long id) {
         return userService.getUserById(id);
     }
@@ -71,7 +73,7 @@ public class UsersController {
             @ApiResponse(responseCode = "200", description = "User was found and updated"),
             @ApiResponse(responseCode = "404", description = "User with such id has not been found")
     })
-    @PutMapping("/users/{id}")
+    @PutMapping(ID)
     public User updateUser(@PathVariable Long id,
                            @RequestBody UserDTO userDTO) {
 
@@ -79,7 +81,7 @@ public class UsersController {
     }
 
     @Operation(summary = "Operation deletes the particular user with given id from the DB")
-    @DeleteMapping("users/{id}")
+    @DeleteMapping(ID)
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUserById(id);
     }
