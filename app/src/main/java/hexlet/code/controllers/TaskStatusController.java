1141f4d82d3e9d7dda0e72a89e314ebe00cf7b0a
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-29 13:13:55 +0200

    feat: add TaskStatusController to the project

---
+++ app/src/main/java/hexlet/code/controllers/TaskStatusController.java
@@ -0,0 +1,89 @@
+package hexlet.code.controllers;
+
+import hexlet.code.dto.TaskStatusDTO;
+import hexlet.code.models.TaskStatus;
+import hexlet.code.services.TaskStatusServiceImpl;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+import static hexlet.code.controllers.TaskStatusController.TASK_STATUS_URL;
+
+@RestController
+@Tag(name = "Task statuses controller")
+@RequestMapping("${base-url}" + TASK_STATUS_URL)
+@RequiredArgsConstructor
+public class TaskStatusController {
+    
+    public static final String TASK_STATUS_URL = "/statuses";
+    
+    private TaskStatusServiceImpl taskStatusService;
+    
+    @Operation(description = "Get all statuses of all tasks")
+    @ApiResponse(responseCode = "200", description = "All task statuses are loaded",
+            content = @Content(schema = @Schema(implementation = TaskStatus.class)))
+    @GetMapping
+    public List<TaskStatus> getAllStatuses() {
+        return taskStatusService.getAllTaskStatuses();
+    }
+    
+    @Operation(description = "Create new task status")
+    @ApiResponse(responseCode = "201", description = "Task status was successfully created")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public TaskStatus createTaskStatus(@RequestBody @Valid final TaskStatusDTO dto) {
+        return taskStatusService.createTaskStatus(dto);
+    }
+    
+    @Operation(description = "Get task status by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task status was successfully loaded"),
+            @ApiResponse(responseCode = "404", description = "Task status was not found")
+    })
+    @GetMapping(path = "/{id}")
+    public TaskStatus getStatusById(@PathVariable("id") final Long id) {
+        return taskStatusService.getTaskStatusById(id);
+    }
+    
+    @Operation(description = "Update user by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task status was successfully updated"),
+            @ApiResponse(responseCode = "404", description = "Task status was not found")
+    })
+    @PutMapping(path = "/{id}")
+    public TaskStatus updateTaskStatus(@PathVariable("id") final Long id,
+                                       @RequestBody @Valid final TaskStatusDTO dto) {
+        return taskStatusService.updateTaskStatus(id, dto);
+    }
+    
+    @Operation(description = "Delete a task status by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task status was deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Task status was not found")
+    })
+    @DeleteMapping(path = "/{id}")
+    public void deleteTaskStatus(@PathVariable("id") final Long id) {
+        taskStatusService.deleteTaskStatus(id);
+    }
+    
+}
