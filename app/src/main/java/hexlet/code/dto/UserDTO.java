3912e1b67e43f68c70e7f2af7e7cfc3ed54e7713
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-29 13:05:10 +0200

    fix: add constructors and validation annotations to UserDTO class

--- app/src/main/java/hexlet/code/dto/UserDTO.java
+++ app/src/main/java/hexlet/code/dto/UserDTO.java
@@ -1,20 +1,42 @@
 package hexlet.code.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
     @Setter(AccessLevel.NONE)
     private long id;
-
+    
+    @NotBlank(message = "First name cannot be empty")
     private String firstName;
-
+    
+    @NotBlank(message = "Last name cannot be empty")
     private String lastName;
-
+    
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email cannot be empty")
     private String email;
-
+    
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 3, max = 255, message = "The password must be 3 to 255 characters long")
     private String password;
+    
+    public UserDTO(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 }
