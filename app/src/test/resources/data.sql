6646b97b22882cd7f66a46ec4b5b4926f1e4cbca
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-29 12:58:46 +0200

    feat: delete unused file

--- app/src/test/resources/data.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM users;
-INSERT INTO users (first_name, last_name, email, password)
-VALUES
-    ('John', 'Smith', 'john@gmail.com', '12345'),
-    ('Jack', 'Doe', 'jack@mail.com', 'qwerty'),
-    ('Jassica', 'Simpson', 'jassika@yahoo.com', '0000'),
-    ('Richard', 'Lionheart', 'Saladdin@gmail.com', 'Acra');
