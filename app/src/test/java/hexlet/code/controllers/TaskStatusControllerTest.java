5d3341eabc0c8f83129eddabbbaeb71367757ae6
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-29 13:14:11 +0200

    feat: add TaskStatusControllerTest class to the project

---
+++ app/src/test/java/hexlet/code/controllers/TaskStatusControllerTest.java
@@ -0,0 +1,79 @@
+package hexlet.code.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import hexlet.code.config.SpringTestConfig;
+import hexlet.code.models.TaskStatus;
+import hexlet.code.repositories.TaskStatusRepository;
+import hexlet.code.services.TaskStatusServiceImpl;
+import hexlet.code.utils.TestUtils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static hexlet.code.config.SpringTestConfig.TEST_PROFILE;
+import static hexlet.code.utils.TestUtils.TEST_USERNAME;
+import static hexlet.code.controllers.TaskStatusController.TASK_STATUS_URL;
+import static hexlet.code.utils.TestUtils.fromJson;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringTestConfig.class)
+@AutoConfigureMockMvc
+@ActiveProfiles(TEST_PROFILE)
+@ExtendWith(SpringExtension.class)
+public class TaskStatusControllerTest {
+    
+    private static final String BASE_URL = "/api";
+    
+    @Autowired
+    private TaskStatusServiceImpl taskStatusService;
+    
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+    
+    @Autowired
+    private TestUtils testUtils;
+    
+    @BeforeEach
+    public void beforeEach() throws Exception {
+        testUtils.tearDown();
+        testUtils.registerDefaultUser();
+    }
+    
+    @AfterEach
+    public void afterEach() {
+        testUtils.tearDown();
+    }
+    
+    @Test
+    public void getAllStatusesTest() throws Exception {
+        
+        final MockHttpServletResponse response = testUtils.perform(
+                get(BASE_URL + TASK_STATUS_URL),
+                        TEST_USERNAME)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        
+        final List<TaskStatus> statuses = fromJson(response.getContentAsString(), new TypeReference<>() {});
+        final List<TaskStatus> expectedStatuses = taskStatusRepository.findAll();
+        
+        Assertions.assertThatList(statuses).isEqualTo(expectedStatuses);
+    }
+    
+    
+    
+}
