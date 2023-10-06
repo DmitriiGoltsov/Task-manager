34e1f2acb862405fc76aec390fbbf4aaadb2f43e
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-10-06 12:31:40 +0200

    fix: add additional validations make fetch type of field "tasks" LAZY.
    1) tasks related to Label are now stored as Set due to their uniqueness;
    2) Adds @JsonIgnore to field "tasks"
    3) Adds property "unique" to the label's name as it should be;
    4) Sets fetch type of tasks field LAZY

--- src/main/java/hexlet/code/models/Label.java
+++ src/main/java/hexlet/code/models/Label.java
@@ -1,5 +1,8 @@
 package hexlet.code.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.Entity;
@@ -13,6 +16,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,25 +24,31 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
-import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "labels")
+@Builder
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Label {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
+
+    @Column(name = "name", unique = true)
     @NotBlank(message = "Name cannot be less than 1 character")
     private String name;
+
     @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     @CreationTimestamp
     private Date createdAt;
-    @ManyToMany(mappedBy = "labels")
-    private List<Task> tasks;
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "labels")
+    private Set<Task> tasks;
 }
