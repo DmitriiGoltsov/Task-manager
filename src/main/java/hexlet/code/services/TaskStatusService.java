8b0fa23609e93290b4cd4d82f6cd179e3fec8849
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-10-14 21:01:34 +0200

    fix: add new contracts to the interface

--- src/main/java/hexlet/code/services/TaskStatusService.java
+++ src/main/java/hexlet/code/services/TaskStatusService.java
@@ -1,7 +1,14 @@
 package hexlet.code.services;
 
+import hexlet.code.dto.TaskStatusDTO;
 import hexlet.code.models.TaskStatus;
+
+import java.util.List;
 
 public interface TaskStatusService {
     TaskStatus getTaskStatusById(Long id);
+    List<TaskStatus> getAllStatuses();
+    TaskStatus createTaskStatus(TaskStatusDTO taskStatusDTO);
+    TaskStatus updateTaskStatus(Long id, TaskStatusDTO taskStatusDTO);
+    void deleteTaskStatus(Long id);
 }
