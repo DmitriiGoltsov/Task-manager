410d32dc39cd0d3b4dd61e34ea0b06aceb29a526
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-10-14 20:57:55 +0200

    fix: improve paths in the Controller

--- src/main/java/hexlet/code/controllers/WelcomeController.java
+++ src/main/java/hexlet/code/controllers/WelcomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/welcome")
+@RequestMapping(path = "")
 public class WelcomeController {
 
-    @GetMapping(path = "")
+    @GetMapping(path = "/welcome")
     public String getGreeting() {
         return "Welcome to Spring";
     }
