0f9ac51fa2c57a09469968a23dacd1cdc39e7c35
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-10-20 13:53:45 +0200

    fix: add additional test cases and put a variable initialization into BeforeAll method

--- src/test/java/hexlet/code/controllers/TaskStatusControllerTest.java
+++ src/test/java/hexlet/code/controllers/TaskStatusControllerTest.java
@@ -11,6 +11,7 @@ import hexlet.code.utils.TestUtils;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,8 +36,12 @@ import static hexlet.code.utils.TestUtils.TEST_USERNAME;
 import static hexlet.code.utils.TestUtils.fromJson;
 import static hexlet.code.utils.TestUtils.asJson;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringTestConfig.class)
@@ -47,6 +52,8 @@ public class TaskStatusControllerTest {
 
     private static final String BASE_URL = "/api";
 
+    private static TaskStatusDTO taskStatusDTO;
+
     @Autowired
     private TaskStatusServiceImplementation taskStatusService;
 
@@ -55,6 +62,11 @@ public class TaskStatusControllerTest {
 
     @Autowired
     private TestUtils testUtils;
+
+    @BeforeAll
+    public static void beforeAll() {
+        taskStatusDTO = new TaskStatusDTO("Test task status");
+    }
 
     @BeforeEach
     public void beforeEach() throws Exception {
@@ -86,16 +98,75 @@ public class TaskStatusControllerTest {
 
     @Test
     public void createNewTaskStatusTest() throws Exception {
-        final TaskStatusDTO taskStatusDTO = new TaskStatusDTO("New task status");
+        TaskStatus savedTaskStatus = createTaskStatusForTest(taskStatusDTO);
+        assertThat(taskStatusRepository.getReferenceById(savedTaskStatus.getId())).isNotNull();
+    }
+
+    @Test
+    public void getTaskStatusByIdTest() throws Exception {
+
+        final TaskStatus expectedTaskStatus = taskStatusRepository.save(TaskStatus.builder()
+                .name("Task status for testing")
+                .build());
+
+        long expectedId = expectedTaskStatus.getId();
+
+        final MockHttpServletRequestBuilder request = get(BASE_URL + TASK_STATUS_URL + "/"
+                + expectedId, expectedId);
+
+        final MockHttpServletResponse response = testUtils.perform(request, TEST_USERNAME)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        final TaskStatus actualTaskStatus = fromJson(response.getContentAsString(), new TypeReference<>() { });
+
+        assertEquals(expectedId, actualTaskStatus.getId());
+        assertEquals(expectedTaskStatus.getName(), actualTaskStatus.getName());
+    }
+
+    @Test
+    public void updateTaskStatusTest() throws Exception {
+
+        final TaskStatus taskStatusToUpdate = createTaskStatusForTest(taskStatusDTO);
+        final long id = taskStatusToUpdate.getId();
+
+        final TaskStatusDTO updatedTaskStatusDTO = new TaskStatusDTO("Updated task status for testing");
+
+        final MockHttpServletRequestBuilder updatingRequest = put(
+                BASE_URL + TASK_STATUS_URL + "/" + id, id)
+                .content(asJson(updatedTaskStatusDTO))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        testUtils.perform(updatingRequest, TEST_USERNAME).andExpect(status().isOk());
+
+        final TaskStatus updatedTaskStatus = taskStatusService.getTaskStatusById(id);
+        assertThat(updatedTaskStatus.getName()).isEqualTo("Updated task status for testing");
+    }
+
+    @Test
+    public void deleteTaskStatusTest() throws Exception {
+
+        final TaskStatus taskStatusToDelete = createTaskStatusForTest(taskStatusDTO);
+        final long id = taskStatusToDelete.getId();
+
+        testUtils.perform(delete(BASE_URL + TASK_STATUS_URL + "/" + id, id), TEST_USERNAME)
+                .andExpect(status().isOk());
+
+        assertFalse(taskStatusRepository.existsById(id));
+    }
+
+    private TaskStatus createTaskStatusForTest(final TaskStatusDTO taskStatusDTO) throws Exception {
+
         final MockHttpServletRequestBuilder request = post(BASE_URL + TASK_STATUS_URL)
                 .content(asJson(taskStatusDTO))
                 .contentType(MediaType.APPLICATION_JSON);
 
-        final MockHttpServletResponse response = testUtils.perform(request, TEST_USERNAME)
-                .andExpect(status().isCreated())
+        final MockHttpServletResponse response = testUtils
+                .perform(request, TEST_USERNAME).andExpect(status().isCreated())
                 .andReturn()
                 .getResponse();
-        final TaskStatus savedTaskStatus = fromJson(response.getContentAsString(), new TypeReference<>() { });
-        assertThat(taskStatusRepository.getReferenceById(savedTaskStatus.getId())).isNotNull();
+
+        return fromJson(response.getContentAsString(), new TypeReference<>() { });
     }
 }
