4f6e116f9fe7fc1196853e4d1eafd5c78e667795
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-10-24 11:52:31 +0200

    fix: remove unused import.

--- src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 @Configuration
 @ComponentScan({"hexlet.code"})
 public class RollbarConfig {
